cc727c40574963b8e4aaa3f14240f88d550348d6
SidhKulkarni <sidhk@live.com>
2019-06-24 14:57:08 -0700

    Configure util files

--- config.js
+++ config.js
@@ -4,9 +4,14 @@ const host = process.env.host || "http://localhost:8000"
 
 module.exports = {
     host: host,
+    issuerServiceUrl: process.env.ISSUER_URL || "https://issuer.trustedkey.com",
+    walletServiceUrl: process.env.WALLET_URL || "https://wallet.trustedkey.com",
     callbackRoute: "/oauth/callback",
     clientID: "",
     clientSecret: "",
     port: process.env.port || 8000,
+    accessTokenUri: 'https://wallet.trustedkey.com/oauth/token',
+    authorizationUri: 'https://wallet.trustedkey.com/oauth/authorize',
+    redirectUri: 'https://localhost:8000/oauth/callback',
     registerScopes: ['openid', 'profile']
 }
--- oauth.js
+++ oauth.js
@@ -1,26 +1,16 @@
-const OpenIDClient = require('openid-client')
+const { Issuer } = require('openid-client')
 const UUID = require('uuid')
-const Config = require("config")
-const host = Config.host
+const Url = require('url')
+const Config = require('./config')
 const clientID = Config.clientID
 const clientSecret = Config.clientSecret
 
-class OpenIDClient{
-    constructor(scopes, flow) {
-        this._scopes= scopes
-        this._flow = flow
-    }
-
-    static getCallbackFlow(req) {
-        return req.query.split(':')[0]
-    }
-
-    async getAuthUri(query, claims) {
-        const nonce = UUID.v4()
-    }
+const getClient = async() => {
+    const issuer = await Issuer.discover(Config.walletServiceUrl)
+    const client = new issuer.client({
+        client_id: clientID,
+        client_secret: clientSecret
+    })
 }
 
-const clients = {
-    "login": new OpenIDClient(["openid"], "login"),
-    "register": new OpenIDClient(Config.registerScopes, "register")
-}
+const client = await getClient
